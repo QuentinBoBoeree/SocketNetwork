f3996a1fdf1e5ae6d127304e1a23c53ecc0793c0
boboeree <icsswin@sina.com>
2017-05-24 01:08:38 +0800

    搭建socketNetwork工程，添加socket-works模块，并添加第一个basic的application

---
+++ socket-works/src/Application.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+/**
+ * Created by quentin on 2017/5/24.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.err.println("first application start in:" + new Date());
+    }
+}
